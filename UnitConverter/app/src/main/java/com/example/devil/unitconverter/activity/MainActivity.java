10fb1cec45bd30cf600b6d7530119a9fa944bb97
D3VIL <D3VIL>
2016-12-31 11:25:25 +0530

    FAB edited.

--- UnitConverter/app/src/main/java/com/example/devil/unitconverter/activity/MainActivity.java
+++ UnitConverter/app/src/main/java/com/example/devil/unitconverter/activity/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Devloped by Anubhav.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
