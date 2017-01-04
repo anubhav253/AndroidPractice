86bf0c045ed4d7309699278bf054ea0daaf8e3c2
D3VIL <D3VIL>
2017-01-04 11:12:07 +0530

    List view added.

---
+++ ListView/app/src/main/java/com/example/devil/listview/SingleListItem.java
@@ -0,0 +1,29 @@
+package com.example.devil.listview;
+
+import android.os.Bundle;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.devil.listview.R;
+
+public class SingleListItem extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.single_list_item_view);
+
+        TextView txtProduct = (TextView) findViewById(R.id.product_label);
+
+        Intent i = getIntent();
+        // getting attached intent data
+        String product = i.getStringExtra("product");
+        // displaying selected product name
+        txtProduct.setText(product);
+
+    }
+}
